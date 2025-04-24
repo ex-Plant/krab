daadc12c1934f380aa345176d5d798ca5b0974f0
ex-Plant <konradantonik@gmail.com>
2025-04-24 13:38:06 +0100

    hide payload template placeholder

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,28 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
-export default PageTemplate
 
-export { generateMetadata }
+export default function Test() {
+
+  return <>
+  <div className={` h-svh flex flex-col   `}>
+    <div className={`grow  flex items-center justify-center`}>
+      <div className={`text-3xl animate-bounce`}>
+        🦀
+      </div>   <div className={`text-3xl animate-bounce delay-100`}>
+        🦀
+      </div>   <div className={`text-3xl animate-bounce delay-200`}>
+        🦀
+      </div>
+    </div>
+    <footer className={` mb-8 text-center `}>
+      Coming soon...
+    </footer>
+
+  </div>
+  </>
+}
+
+
+// export default PageTemplate
+//
+// export { generateMetadata }
